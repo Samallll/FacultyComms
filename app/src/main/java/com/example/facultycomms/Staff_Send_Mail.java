a5a02b5aeb3fc94ad73ab6251cf83040bc7cedb6
samalchokli@gmail.com <samalchokli@gmail.com>
2022-12-08 10:56:44 +0530

    Staff send mail UI configured

---
+++ app/src/main/java/com/example/facultycomms/Staff_Send_Mail.java
@@ -0,0 +1,13 @@
+package com.example.facultycomms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Staff_Send_Mail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_send_mail);
+    }
+}
